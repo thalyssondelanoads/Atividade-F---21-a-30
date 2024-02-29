881a38f0e3a438428c390d4ca96e4b2e1150df99
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-28 23:48:03 -0300

    Create questao21.py

---
+++ questao21.py
@@ -0,0 +1,22 @@
+def arredondar():
+    from math import ceil, floor
+    
+    meio = num + 0.5
+    sucessor = num // 1 + 1
+    
+    if meio >= sucessor:
+        pra_maior = ceil(num)
+        print(F' O Número foi aproximado para {pra_maior} ')
+        
+    else:
+        pra_menor = floor(num)
+        print(F' O Número foi aproximado para {pra_menor} ')
+#----------------------------
+
+print('Arredondar Números')
+print('-------------')
+
+num = float(input('Digite o Número que Deseja Arredondar: '))
+print('-------------')
+
+arredondar()
