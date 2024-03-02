9cf7723da5b3c333d0bb1f5420d009180aee152a
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-03-01 21:01:27 -0300

    Atualizar o questao28.py

--- questao28.py
+++ questao28.py
@@ -1,1 +1,19 @@
-.
+def area():
+    area = x * y
+    
+    if area < 0:
+        print(' A Área NÃO pode ser Negativa')
+        
+    else:
+        print(f' A Área do Retângulo é {area} ')
+
+#---------------------------
+
+print('Área do Retângulo no Plano')
+print('----------------')
+
+x = int(input('Digite o Valor de X no Plano:  '))
+y = int(input('Digite o Valor de Y no Plano:  '))
+print('----------------')
+
+area()
